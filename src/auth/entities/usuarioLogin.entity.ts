b99b53c6fa51318da662f15c0ed3f326b36f5b67
IgorCavalcantiMoura <icavalcantimoura@gmail.com>
2024-10-30 19:56:21 -0300

    :rocket: deploy

--- src/auth/entities/usuarioLogin.entity.ts
+++ src/auth/entities/usuarioLogin.entity.ts
@@ -3,19 +3,19 @@ import { IsNotEmpty, IsString, MinLength } from 'class-validator';
 
 export class UsuarioLogin {
   @ApiProperty({
-    description: 'Nome de usuário ou e-mail para login',
-    example: 'email@email.com.br',
+    // description: 'Nome de usuário ou e-mail para login',
+    // example: 'email@email.com.br',
   })
-  @IsString({ message: 'O nome de usuário deve ser uma string.' })
-  @IsNotEmpty({ message: 'O nome de usuário não pode estar vazio.' })
+  // @IsString({ message: 'O nome de usuário deve ser uma string.' })
+  // @IsNotEmpty({ message: 'O nome de usuário não pode estar vazio.' })
   public usuario: string;
 
   @ApiProperty({
-    description: 'Senha do usuário (mínimo de 8 caracteres)',
-    example: 'senhaSegura123',
+    // description: 'Senha do usuário (mínimo de 8 caracteres)',
+    // example: 'senhaSegura123',
   })
-  @IsString({ message: 'A senha deve ser uma string.' })
-  @IsNotEmpty({ message: 'A senha não pode estar vazia.' })
-  @MinLength(8, { message: 'A senha deve ter pelo menos 8 caracteres.' })
+  // @IsString({ message: 'A senha deve ser uma string.' })
+  // @IsNotEmpty({ message: 'A senha não pode estar vazia.' })
+  // @MinLength(8, { message: 'A senha deve ter pelo menos 8 caracteres.' })
   public senha: string;
 }
