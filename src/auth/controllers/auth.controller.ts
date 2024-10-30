150fc50d2a318f02e9deed1e4177898c4f807373
IgorCavalcantiMoura <icavalcantimoura@gmail.com>
2024-10-29 22:52:32 -0300

    :rockets: Deploy: Ajustes Deploy

--- src/auth/controllers/auth.controller.ts
+++ src/auth/controllers/auth.controller.ts
@@ -1,7 +1,7 @@
 import { Body, Controller, HttpCode, HttpStatus, Post, UseGuards } from '@nestjs/common';
 import { LocalAuthGuard } from '../guard/local-auth.guard';
 import { AuthService } from '../services/auth.service';
-import { UsuarioLogin } from './../entities/usuariologin.entity';
+import { UsuarioLogin } from '../entities/usuariologin.entity';
 import { ApiBody, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 @ApiTags('Usuario')
 @Controller("/usuarios")
